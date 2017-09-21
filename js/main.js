41a6f7f770ee9bd07024c2a7f5dc63da793783bb
Weichao Zhao <weichao.zhao@stonybrook.edu>
2017-09-21 01:34:29 -0400

    add progress bar, and will test the play function later

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+function playSong()
+{
+  //get the current time and then / totaltime
+  //var t = document.get
+  var times = S.split(":");
+  var minutes = times[0];
+  var seconds = times[1];
+  seconds = parseInt(seconds, 10) + (parseInt(minutes, 10) * 60);
+}
